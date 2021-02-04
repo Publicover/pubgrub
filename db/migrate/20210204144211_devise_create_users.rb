2d1b24a784f05ce04be6a0505bd83d422ed03902
Jim Publicover <jameslpublicover@gmail.com>
2021-02-04 15:47:54 +0100

    whoops, roles

--- db/migrate/20210204144211_devise_create_users.rb
+++ db/migrate/20210204144211_devise_create_users.rb
@@ -33,6 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :fname
+      t.integer :role
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_144211) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "fname"
+    t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
