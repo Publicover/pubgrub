041972d5181c8368cdfe140c3be90cbec195b9cc
Jim Publicover <jameslpublicover@gmail.com>
2021-02-04 15:45:59 +0100

    devise

---
+++ test/controllers/dashboards_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  test 'should redirect if not logged in' do
+    get root_path
+    assert_redirected_to user_session_path
+  end
+
+  test 'should log in' do
+    login_as users(:jim)
+    get root_path
+    assert_response :success
+  end
+end
